1f5f7b95d1c2146f883c51848b85fa30551d9052
Gimrotl <116345351+Gimrotl@users.noreply.github.com>
2025-12-28 01:26:51 +0100

    Updated CarTypeIcon.tsx

--- src/components/CarTypeIcon.tsx
+++ src/components/CarTypeIcon.tsx
@@ -20,13 +20,14 @@ export const CarTypeIcon: React.FC<CarTypeIconProps> = ({
         viewBox="0 0 64 64"
         fill="none"
         xmlns="http://www.w3.org/2000/svg"
-        className="text-slate-500"
+        className="text-blue-600"
       >
-        <path d="M16 40C14.9 40 14 40.9 14 42C14 43.1 14.9 44 16 44C17.1 44 18 43.1 18 42C18 40.9 17.1 40 16 40Z" fill="currentColor"/>
-        <path d="M48 40C46.9 40 46 40.9 46 42C46 43.1 46.9 44 48 44C49.1 44 50 43.1 50 42C50 40.9 49.1 40 48 40Z" fill="currentColor"/>
-        <path d="M50 18H14C12.89 18 12 18.9 12 20V38C12 39.1 12.89 40 14 40H16V36H18V40H46V36H48V40H50C51.1 40 52 39.1 52 38V20C52 18.9 51.1 18 50 18ZM50 36H14V20H50V36Z" fill="currentColor" opacity="0.7"/>
-        <path d="M26 22H20C19.4 22 19 22.4 19 23V32C19 32.6 19.4 33 20 33H26C26.6 33 27 32.6 27 32V23C27 22.4 26.6 22 26 22Z" fill="currentColor" opacity="0.5"/>
-        <path d="M44 22H38C37.4 22 37 22.4 37 23V32C37 32.6 37.4 33 38 33H44C44.6 33 45 32.6 45 32V23C45 22.4 44.6 22 44 22Z" fill="currentColor" opacity="0.5"/>
+        <path d="M10 42C8.9 42 8 42.9 8 44C8 45.1 8.9 46 10 46C11.1 46 12 45.1 12 44C12 42.9 11.1 42 10 42Z" fill="currentColor"/>
+        <path d="M54 42C52.9 42 52 42.9 52 44C52 45.1 52.9 46 54 46C55.1 46 56 45.1 56 44C56 42.9 55.1 42 54 42Z" fill="currentColor"/>
+        <path d="M56 28H52C52 22 48.6 17 44 16H20C15.4 17 12 22 12 28H8C6.9 28 6 28.9 6 30V42C6 43.1 6.9 44 8 44H12V40H52V44H56C57.1 44 58 43.1 58 42V30C58 28.9 57.1 28 56 28ZM56 40H8V30H56V40Z" fill="currentColor" opacity="0.8"/>
+        <path d="M20 18C20 18 18 20 18 24C18 25.1 18.9 26 20 26H28C28.6 26 29 25.6 29 25V18H20Z" fill="currentColor" opacity="0.6"/>
+        <path d="M44 18V25C44 25.6 44.4 26 45 26H53C54.1 26 55 25.1 55 24C55 20 53 18 44 18Z" fill="currentColor" opacity="0.6"/>
+        <path d="M30 22H34C34.55 22 35 21.55 35 21C35 20.45 34.55 20 34 20H30C29.45 20 29 20.45 29 21C29 21.55 29.45 22 30 22Z" fill="currentColor" opacity="0.4"/>
       </svg>
     </div>
   );
