f0401ea0b435d0a6a9596219eecb5cba75ac63fe
Gimrotl <116345351+Gimrotl@users.noreply.github.com>
2025-12-29 21:52:44 +0100

    Updated translations.ts

--- src/data/translations.ts
+++ src/data/translations.ts
@@ -353,7 +353,7 @@ export const translations = {
     fromPlaceholder: 'From (e.g., Grozny)',
     toPlaceholder: 'To (e.g., Mecca)',
     createAccount: 'Create Account',
-    signIn: 'Sign In п',
+    signIn: 'Sign Inh',
     email: 'Email',
     yourEmail: 'Your email',
     password: 'Password',
